fb446ba73bc96ba1bad56226d05b7a3268044eed
bechirsaidi <bachirbobsaidi@gmail.com>
2022-04-08 12:51:15 +0200

    first commit

---
+++ src/Classe/Mail.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Classe;
+
+use Mailjet\Client;
+use Mailjet\Resources;
+use Symfony\Component\Dotenv\Dotenv;
+
+class Mail
+{
+    private $api_key;
+    private $api_key_secret;
+
+    public function __construct()
+    {
+        (new Dotenv())->bootEnv(dirname(__DIR__) . '/../.env');
+        $this->api_key = $_ENV['API_KEY'];
+        $this->api_key_secret = $_ENV['API_KEY_SECRET'];
+    }
+
+    public function send($to_email, $to_name, $subject, $content)
+    {
+        $mj = new Client($this->api_key, $this->api_key_secret, true, ['version' => 'v3.1']);
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => "bachirbobsaidi@gmail.com",
+                        'Name' => "bechir"
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $to_email,
+                            'Name' => $to_name
+                        ]
+                    ],
+                    'TemplateID' => 3814931,
+                    'TemplateLanguage' => true,
+                    'Subject' => $subject,
+                    'Variables' => [
+                        'content' => $content
+                    ]
+                ]
+            ]
+        ];    
+                
+            
+
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success();
+    }
+}
